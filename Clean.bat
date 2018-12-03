5446d219f47da88ac45a06e287b832118e78737a
ljmdzyx1985 <45554505+ljmdzyx1985@users.noreply.github.com>
2018-12-03 15:57:24 +0800

    Add files via upload

---
+++ Clean.bat
@@ -0,0 +1,28 @@
+del *.aux /s
+del *.bak /s
+del *.log /s
+
+del *.bbl /s
+del *.dvi /s
+del *.blg /s
+del *.thm /s
+del *.toc /s
+del *.toe /s
+
+del *.lof 
+del *.lot
+del *.out 
+
+del *.fen
+del *.ten
+del *.ps
+
+del *.gz
+del *.gz(busy)
+del *.fdb_latexmk /s
+del *.synctex
+del *.synctex.gz /s
+
+del *.loa
+del *.nav  
+del *.snm 
